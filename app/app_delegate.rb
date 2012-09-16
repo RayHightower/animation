7ed89f5464d54d3f49ca94bc3113fc930a73e2ef
Raymond T. Hightower <rth@wisdomgroup.com>
2012-09-16 18:18:07 -0500

    defining points in app_delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,19 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    @window.makeKeyAndVisible
+
+    # the points we're going to animate to
+    @points = [[0, 0], [50, 0], [0, 50], [50, 50]]
+    @current_index = 0
+
+    # usual method of adding views to our window
+    @view = UIView.alloc.initWithFrame [@points[@current_index], [100, 100]]
+    @view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@view)
+
+    animate_to_next_point
+      
     true
   end
 end
